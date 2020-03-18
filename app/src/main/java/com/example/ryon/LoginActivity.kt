0356f754a4645e885e3d6043c72be6783e3f5ddb
Hoong Kah Fai <hoongkf-wm17@student.tarc.edu.my>
2020-03-18 16:00:25 +0800

    hi

---
+++ app/src/main/java/com/example/ryon/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.example.ryon
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class LoginActivity:AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.)
+    }
+}
---
+++ app/src/main/java/com/example/ryon/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.ryon
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        register_button_register.setOnClickListener {
+        val email = email_edittext_register.text.toString()
+        val password = password_edittext_register.text.toString()
+
+        Log.d("MainActivity","Email is:"+email)
+        Log.d("MainActivity","Password $password")
+        }
+
+        already_have_account_textview.setOnClickListener {
+            Log.d("MainActivity","Try to show login activity")
+
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
